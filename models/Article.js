f194e9cb8f89a081332e405d1adb819c25d96a4a
Dmitry <dmitryprokoptsov@gmail.com>
2023-06-28 08:22:06 -0700

    first init

---
+++ models/Article.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+    },
+    content: {
+        type: String,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    media: {
+        type: String,
+        required: false,  // Since not every post may have media
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    comments: [{
+        commenter: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        content: {
+            type: String,
+        },
+        at: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    postDate: {
+        type: Date,
+        default: Date.now
+    }
+
+
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
